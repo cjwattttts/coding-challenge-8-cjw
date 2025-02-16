79691f23c976d62cbe4406cb4e12923e7cdd2bad
cjwattttts <cameron.jwatts@outlook.com>
2025-02-16 12:35:30 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); // Expected output: "Net Salary: $6950.00"
+
+// TASK 2
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate); // applies discount to price
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+};
+
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15)); // Expected output: "Final Price: $212.50"
